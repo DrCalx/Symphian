9aa52e28584a258dbafd25fb5181fe5aea68f93f
Matthew Duguay <matthew.duguay@gmail.com>
2014-07-07 17:31:12 -0400

    Initialize Symphian as default Rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Symphian::Application.config.secret_key_base = 'eb6fe39eadf62d4ce933791e2efb7f0fb7cc531d285dc981f43aec9eb7fe32a14b4f398e13dbae0983e1392b28601f8154a68937755fb28c72530c71552d28b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Symphian::Application.config.session_store :cookie_store, key: '_Symphian_session'
