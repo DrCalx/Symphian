3c93249edae21a7e81d59ca30811365fbe8111cb
Matthew Duguay <matthew.duguay@gmail.com>
2014-07-17 18:26:00 -0400

    Add static pages controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
 	has_many :memberships, dependent: :destroy
 	has_many :groups, through: :memberships
+
+	def sign_with!(group)
+		self.memberships.create!(group.id)
+	end
+
+	def signed_with?(group)
+		self.memberships.find_by(group.id)
+	end
+
+	def unsign_with!(group)
+		self.memberships.find_by(group.id).destroy
+	end
 end
