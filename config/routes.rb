df3405a3e6375498e5f5e86d078b31c0e2cf9243
Matthew Duguay <matthew.duguay@gmail.com>
2014-08-01 11:57:06 -0400

    Add rudimentary About page. Fix links on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Symphian::Application.routes.draw do
   resources :users
   resources :sessions
   root 'static_pages#home'
+  match '/about', to: 'static_pages#about', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
   match '/signin', to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
