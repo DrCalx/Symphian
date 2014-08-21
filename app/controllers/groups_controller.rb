4dd3724df82f4959d011368c7cb33734a4738422
Matthew Duguay <matthew.duguay@gmail.com>
2014-08-21 18:34:17 -0400

    Working group member form!

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,10 @@ class GroupsController < ApplicationController
 	def show
 		@group = Group.find(params[:id])
 	end
+
+	def update
+		@group = Group.find(params[:id])
+		@group.add_member!(params[:group][:members])
+		redirect_to @group
+	end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ActiveRecord::Base
 	has_many :memberships, dependent: :destroy
 	has_many :members, through: :memberships, source: :user
 
-	def add_member!(user)
-		self.memberships.create(user.id)
+	def add_member!(user_id)
+		self.memberships.create(user_id: user_id)
 	end
 end
