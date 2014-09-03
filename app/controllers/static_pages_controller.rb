2905a650b8a84e60a620004b55d3da97b0b1d5fc
Matthew Duguay <matthew.duguay@gmail.com>
2014-09-03 18:32:15 -0400

    Fix Broken Homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-  	@user = User.find("1")
-  	@favtracks = @user.soundcloud_client.get("/me/tracks", :limit => 5)
   end
 
   def about
