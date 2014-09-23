7a907a6a73b3a9d7fae5001c6773360bf0f2f472
Matthew Duguay <matthew.duguay@gmail.com>
2014-09-23 18:40:10 -0400

    Create Homeblocks. Basic home layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+  	if signed_in?
+  		@openings = Opening.where(:zip => current_user.zip, :instrument_id => @quack)
+  	end
   end
 
   def about
