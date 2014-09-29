579e19b3b6f6cf57cc31e81f26cb8c5899782060
Matthew Duguay <matthew.duguay@gmail.com>
2014-09-29 16:15:21 -0400

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924185853) do
+ActiveRecord::Schema.define(version: 20140928230352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,20 @@ ActiveRecord::Schema.define(version: 20140924185853) do
   create_table "instruments", force: true do |t|
     t.string   "name"
     t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "listing_types", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "listings", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "listing_type_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
