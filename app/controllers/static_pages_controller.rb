c1d4e5f3deaf4b29a88e4c9996801269ecfc1fd1
Matthew Duguay <matthew.duguay@gmail.com>
2014-10-10 20:14:44 -0400

    Listings tabs and feed on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
   	if signed_in?
   		@openings = Opening.where(:zip => current_user.zip, :instrument_id => @quack)
+  		@listings = Listing.all
   	end
   end
 
