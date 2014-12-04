cc485b747aa020222e033ef8dfbb01a776062939
drcalx <matthew.duguay@gmail.com>
2014-12-04 16:45:40 +0000

    Test Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ActiveRecord::Base
 	belongs_to :listing_type
-	
+	validates :listing_type, presence: true
 	belongs_to :user
+	validates :user_id, presence: true
+	validates :description, presence: true
 end
