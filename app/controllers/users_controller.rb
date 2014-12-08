42e49ea994abd7b23cf5376ad0812eaef82d5511
drcalx <matthew.duguay@gmail.com>
2014-12-08 20:10:57 +0000

    User signin tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :ensure_signed_in, only: [:update]
+	
 	def show
 		@user = User.find(params[:id])
 		@instruments = @user.instruments
@@ -41,5 +43,12 @@ class UsersController < ApplicationController
 		def user_params
 			#params is passed in via the form. We need to insure only the correct hash values are present
 			params.require(:user).permit(:name, :email, :password, :password_confirmation, :zip, :bio, :instrument_ids => [], :genre_ids => [])
+		end
+		
+		def ensure_signed_in
+			unless signed_in?
+				flash[:danger] = "Please log in."
+				redirect_to signin_url
+			end
 		end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:matt)
+  end
+  
   test "should get new" do
     get :new
     assert_response :success
+  end
+  
+  test "update invalid without signin" do
+    patch :update, id: @user, user: { name: @user.name,
+                                      email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to signin_url
   end
 end
