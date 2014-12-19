ad9aa87bfc1cff5122ec32765e9ac0894219a66a
drcalx <matthew.duguay@gmail.com>
2014-12-19 19:11:20 +0000

    Venue create and show controller views

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,4 +2,23 @@ class VenuesController < ApplicationController
   def new
     @venue = Venue.new
   end
+  
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to @venue
+    else
+      flash[:error] = "Couldn't save"
+      render 'new'
+    end
+  end
+  
+  def show
+    @venue = Venue.find(params[:id])
+  end
+  
+  private
+  def venue_params
+    params.require(:venue).permit(:name)
+  end
 end
