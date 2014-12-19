7b5566c80293a5270fe6a551378c36c0cd9217fa
drcalx <matthew.duguay@gmail.com>
2014-12-19 19:50:32 +0000

    Admin column added to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217212208) do
+ActiveRecord::Schema.define(version: 20141219194915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 20141217212208) do
     t.string   "soundcloud_access_token",  limit: 255
     t.string   "soundcloud_refresh_token", limit: 255
     t.datetime "soundcloud_expires_at"
+    t.boolean  "admin",                                default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
