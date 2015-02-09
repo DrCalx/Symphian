dc6dc331c4d78313dc899491f195d40a1b7fcd7d
drcalx <matthew.duguay@gmail.com>
2015-02-09 22:02:46 +0000

    minor migration fix

--- db/migrate/20150209193811_move_password_from_user_to_auth_symphian.rb
+++ db/migrate/20150209193811_move_password_from_user_to_auth_symphian.rb
@@ -1,7 +1,7 @@
 class MovePasswordFromUserToAuthSymphian < ActiveRecord::Migration
   def change
     User.find_each do |u|
-      u.auth_symphian.find_or_initialize_by_user_id(user_id: u.id, password_digest: u.password_digest)
+      u.auth_symphian.find_or_initialize_by(user_id: u.id, password_digest: u.password_digest)
       u.save!
     end
   end
