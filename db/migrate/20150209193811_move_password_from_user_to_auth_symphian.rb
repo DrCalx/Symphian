ce5150d6e6c85447fcd07714fb71fb5e9f3260ec
drcalx <matthew.duguay@gmail.com>
2015-02-09 22:12:25 +0000

    Migration checks for nil auth class

--- db/migrate/20150209193811_move_password_from_user_to_auth_symphian.rb
+++ db/migrate/20150209193811_move_password_from_user_to_auth_symphian.rb
@@ -1,7 +1,9 @@
 class MovePasswordFromUserToAuthSymphian < ActiveRecord::Migration
   def change
     User.find_each do |u|
-      u.auth_symphian.find_or_initialize_by(user_id: u.id, password_digest: u.password_digest)
+      if u.auth_symphian.nil?
+        u.build_auth_symphian(user_id: u.id, password_digest: u.password_digest)
+      end
       u.save!
     end
   end
