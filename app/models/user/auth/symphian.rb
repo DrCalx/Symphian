61ee6030284389e8e2a05a0a859e3cdab784b0a8
drcalx <matthew.duguay@gmail.com>
2015-02-10 19:11:41 +0000

    Securitease

--- app/models/user/auth/symphian.rb
+++ app/models/user/auth/symphian.rb
@@ -1,6 +1,6 @@
 class User::Auth::Symphian < ActiveRecord::Base
   belongs_to :user
- # has_secure_password
+  has_secure_password
   	#validates :password, length: { minimum: 6 }, 
 	#											on: :create
 end
